1ffb411c2f4edc0cb4513051ea534a94862f07b4
Muhammad Idrees <m.idris991986@gmail.com>
2020-12-29 09:20:30 -0800

    Compeleted Task2 of Javascript Assignment2

---
+++ Task2.js
@@ -0,0 +1,13 @@
+var Signal = prompt("What is the color of road traffic signal?");
+if (Signal==="Red"){
+    alert("Must Stop");
+}
+    if (Signal==="Yellow"){
+        alert("Ready to Move");
+    }
+        if(Signal==="Green"){
+            alert("Move now");
+        }
+else{
+    alert("Road traffic signal can only be Red / Yellow / Green, Please give correct input")
+}
