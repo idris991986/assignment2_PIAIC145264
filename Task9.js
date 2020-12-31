17ab6fe6673490b6b017a367dcce306867934f30
Muhammad Idrees <m.idris991986@gmail.com>
2020-12-31 07:17:34 -0800

    Completed Task 9 of Javascript Assignment 2

---
+++ Task9.js
@@ -0,0 +1,7 @@
+var Character = prompt("Please enter the character of your choice?","a to z");
+if((Character == "a") || (Character == "e") || (Character == "i") || (Character == "o") || (Character == "u")){
+    alert("True");
+}
+else {
+    alert("False");
+}
